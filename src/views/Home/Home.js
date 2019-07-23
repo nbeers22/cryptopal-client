0679a57532b344d0220385e9b09e484e03697928
nbeers22 <nbeers22@gmail.com>
2019-07-23 12:59:38 -0400

    Created skeleton and fake content for home hero and features sections

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import './Home.css';
 
 export default class Home extends Component {
@@ -9,6 +11,28 @@ export default class Home extends Component {
           <div className="container">
           <h1>CryptoPal</h1>
           <p className="intro-subhead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure, fugiat! Commodi, blanditiis ipsa provident illum consectetur explicabo repudiandae molestias consequuntur quos, dicta cumque necessitatibus sint labore unde illo pariatur tempore?</p>
+          <p className="intro-subhead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa illum consectetur explicabo repudiandae molestias consequuntur quos, dicta cumque necessitatibus sint labore unde illo pariatur tempore?</p>
+          </div>
+        </section>
+        <section className="features">
+          <div className="container">
+            <div className="feature-container">
+              <aside className="feature">
+                <FontAwesomeIcon icon={faCoffee} />
+                <h2>Feature Name</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure architecto exercitationem facere nesciunt beatae omnis quibusdam eligendi voluptas iusto alias quod, quo, ullam </p>
+              </aside>
+              <aside className="feature">
+                <FontAwesomeIcon icon={faCoffee} />
+                <h2>Feature Name</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure architecto exercitationem facere nesciunt beatae omnis quibusdam eligendi voluptas iusto alias quod, quo, ullam </p>
+              </aside>
+              <aside className="feature">
+                <FontAwesomeIcon icon={faCoffee} />
+                <h2>Feature Name</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure architecto exercitationem facere nesciunt beatae omnis quibusdam eligendi voluptas iusto alias quod, quo, ullam </p>
+              </aside>
+            </div>
           </div>
         </section>
       </div>
