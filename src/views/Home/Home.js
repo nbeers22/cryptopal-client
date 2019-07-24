2199a00d2ab89c2977ad4109a800ae397992c50c
nbeers22 <nbeers22@gmail.com>
2019-07-24 15:22:19 -0400

    Updated Home page view CTAs

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import './Home.css';
@@ -15,24 +16,38 @@ export default class Home extends Component {
           </div>
         </section>
         <section className="features">
-          <div className="container">
-            <div className="feature-container">
-              <aside className="feature">
-                <FontAwesomeIcon icon={faCoffee} />
-                <h2>Feature Name</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure architecto exercitationem facere nesciunt beatae omnis quibusdam eligendi voluptas iusto alias quod, quo, ullam </p>
-              </aside>
-              <aside className="feature">
-                <FontAwesomeIcon icon={faCoffee} />
-                <h2>Feature Name</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure architecto exercitationem facere nesciunt beatae omnis quibusdam eligendi voluptas iusto alias quod, quo, ullam </p>
-              </aside>
-              <aside className="feature">
-                <FontAwesomeIcon icon={faCoffee} />
-                <h2>Feature Name</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure architecto exercitationem facere nesciunt beatae omnis quibusdam eligendi voluptas iusto alias quod, quo, ullam </p>
-              </aside>
-            </div>
+          <div className="feature-container">
+            <aside className="feature">
+              <FontAwesomeIcon icon={faCoffee} />
+              <h2>Feature Name</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure architecto exercitationem facere nesciunt beatae omnis quibusdam eligendi voluptas iusto alias quod, quo, ullam </p>
+            </aside>
+            <aside className="feature">
+              <FontAwesomeIcon icon={faCoffee} />
+              <h2>Feature Name</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure architecto exercitationem facere nesciunt beatae omnis quibusdam eligendi voluptas iusto alias quod, quo, ullam </p>
+            </aside>
+            <aside className="feature">
+              <FontAwesomeIcon icon={faCoffee} />
+              <h2>Feature Name</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure architecto exercitationem facere nesciunt beatae omnis quibusdam eligendi voluptas iusto alias quod, quo, ullam </p>
+            </aside>
+          </div>
+        </section>
+        <section className="ctas">
+          <div className="feature-container">
+            <aside className="cta">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem natus voluptates cupiditate autem itaque quas tempora pariatur, distinctio, nostrum earum maiores repellendus illo ipsa est cumque nulla placeat reprehenderit veritatis.</p>
+              <button type="button">
+                <Link to="/coins">View coins</Link>  
+              </button>              
+            </aside>
+            <aside className="cta">
+              <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem natus voluptates cupiditate autem itaque quas tempora pariatur, distinctio, nostrum earum maiores repellendus illo ipsa est cumque nulla placeat reprehenderit veritatis.</p>
+              <button type="button">
+                <Link to="">Sign up</Link>  
+              </button>              
+            </aside>
           </div>
         </section>
       </div>
