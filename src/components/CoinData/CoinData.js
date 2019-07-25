24249339648a3760556e2f300e08d92de8f01820
nbeers22 <nbeers22@gmail.com>
2019-07-25 16:40:23 -0400

    Added second API call to server and changed fetch to Promise.all for mutiple fetches

--- src/components/CoinData/CoinData.js
+++ src/components/CoinData/CoinData.js
@@ -24,50 +24,76 @@ class CoinData extends Component {
         description: "",
         tags: []
       }
+    },
+    coinMarket: {
+      1: {
+        cmc_rank: null,
+        max_supply: null,
+        circulating_supply: null,
+        quote: {
+          USD: {
+            market_cap: null,
+            percent_change_1h: null,
+            percent_change_24h: null,
+            percent_change_7d: null,
+            price: null,
+            volume_24h: null
+          }
+        },
+        
+      }
     }
   }
   
   componentDidMount(){
     const coinID = this.props.match.params.coin_id;
-    const url = `${config.API_URL}/coins/${coinID}`;
-    fetch(url)
-    .then( response => {
-      return response.ok
-        ? response.json()
-        : console.log(response.statusText)
-    })
-    .then( responseJson => {
-      this.setState({
-        coinData: responseJson.data
+    const urls = [
+      `${config.API_URL}/coins/${coinID}`,
+      `${config.API_URL}/coins/${coinID}/market`
+    ];
+    Promise.all(urls.map(url => fetch(url)))
+      .then(responses => Promise.all(
+        responses.map(response => response.json())
+      ))
+      .then(data => {
+        this.setState({
+          coinData: data[0].data,
+          coinMarket: data[1].data
+        })
       })
-    })
-    .catch( error => console.log(error))
   }
 
   render() {
-    const { name, urls, logo, symbol, description, tags  } = this.state.coinData[1];
-
+    const { name, urls, logo, symbol, description, tags } = this.state.coinData[1];
+    const { cmc_rank, max_supply, quote } = this.state.coinMarket[1];
+    console.log(this.state.coinMarket[1])
     return (
-      <section className="CoinData">
+      
+      <div className="CoinData">
         <div className="container">
           <section className="CoinData-hero">
             <img src={logo} alt=""/>
             <h1>{name}</h1>
+            <p>{`Rank #${cmc_rank}`}</p>
             <p className="description">{description}</p>
           </section>
-          <aside className="coin-links">
-            <h3>Links</h3>
-            <p><a href={urls.website} target="_blank">Website</a></p>
-            <ul>
-              <li><a href={urls.reddit} target="_blank"><FontAwesomeIcon icon={faReddit} /></a></li>
-              <li><a href={urls.reddit} target="_blank"><FontAwesomeIcon icon={faTwitter} /></a></li>
-            </ul>
-            <p><a href={urls.website}>Website</a></p>
-            <p><a href={urls.website}>Website</a></p>
-            <p><a href={urls.website}>Website</a></p>
-          </aside>
+          <section className="CoinData-info">
+            <div className="container">
+              <aside className="coin-links">
+                <h3>Links</h3>
+                <p><a href={urls.website} target="_blank">Website</a></p>
+                <ul>
+                  <li><a href={urls.reddit} target="_blank"><FontAwesomeIcon icon={faReddit} /></a></li>
+                  <li><a href={urls.twitter} target="_blank"><FontAwesomeIcon icon={faTwitter} /></a></li>
+                </ul>
+                <p><a href={urls.website}>Website</a></p>
+                <p><a href={urls.website}>Website</a></p>
+                <p><a href={urls.website}>Website</a></p>
+              </aside>
+            </div>
+          </section>
         </div>
-      </section>
+      </div>
     )
   }
 }
