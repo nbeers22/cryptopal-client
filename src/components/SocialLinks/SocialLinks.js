9c0af0a763665603cf7636d3fbc5baaf7af273d6
nbeers22 <nbeers22@gmail.com>
2019-07-26 09:53:43 -0400

    Created SocialLinks component to display on coin show page

---
+++ src/components/SocialLinks/SocialLinks.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faReddit, faTwitter } from '@fortawesome/free-brands-svg-icons'
+
+function SocialLinks(props){
+  let reddit,twitter;
+
+  if(props.twitter){
+    twitter = <li className="twitter"><a href={props.twitter} target="_blank"><FontAwesomeIcon icon={faTwitter} /></a></li>
+  }
+  if(props.reddit){
+    reddit = <li className="reddit"><a href={props.reddit} target="_blank"><FontAwesomeIcon icon={faReddit} /></a></li>
+  }
+  return(
+    <ul className="social">
+      {reddit}
+      {twitter}
+    </ul>
+  )
+}
+
+export default SocialLinks;
