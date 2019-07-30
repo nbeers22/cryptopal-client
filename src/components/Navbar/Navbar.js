848f753d61d1b480b89511963a0a0e19a4e96393
nbeers22 <nbeers22@gmail.com>
2019-07-30 14:51:12 -0400

    Created different links for navabr depending whether or not logged in

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,17 +3,30 @@ import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar() {
+  let navbarItems;
+
+  if(window.localStorage.getItem('cryptopal-client-auth-token')){
+    navbarItems = 
+      <ul>
+        <li><Link to="/dashboard">Dashboard</Link></li>
+        <li><Link to="/account">Account</Link></li>
+      </ul>
+  }else{
+    navbarItems = 
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/signup">Sign up</Link></li>
+        <li><Link to="/login">Sign in</Link></li>
+      </ul>
+  }
+
   return(
     <nav>
       <div className="top-bar-left">
         <Link to="/"><img src="" alt="Logo"/></Link>
       </div>
       <div className="top-bar-right">
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><a href="#!">Link</a></li>
-          <li><a href="#!">Link</a></li>
-        </ul>
+        {navbarItems}
       </div>
     </nav>
   )
