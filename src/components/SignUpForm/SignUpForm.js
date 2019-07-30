1f0f96491fb531845d923d7017961eb34f1b989d
nbeers22 <nbeers22@gmail.com>
2019-07-30 13:29:22 -0400

    Put in link for users that have already signed up

--- src/components/SignUpForm/SignUpForm.js
+++ src/components/SignUpForm/SignUpForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import FlashMessage from '../FlashMessage/FlashMessage.js';
 import config from '../../config.js';
 import './SignUpForm.css';
@@ -92,6 +93,7 @@ export default class SignUpForm extends Component {
               <input type="submit" value="Submit" />
             </div>
           </form>
+          <p>Already have an account? <Link to="/login">Sign in</Link></p>
         </div>
       </section>
     )
