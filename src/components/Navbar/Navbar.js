e419e9df71e43936307b2bba386f1fcc86e86faa
nbeers22 <nbeers22@gmail.com>
2019-07-31 13:36:02 -0400

    Changed Navbar check to sessionStorage

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,15 +1,25 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, withRouter } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
 import './Navbar.css';
+import TokenService from '../../services/token-service';
 
-function Navbar() {
+function Navbar(props) {
   let navbarItems;
+  const gravatarURL = TokenService.getAuthGravatar();
+  const userName = TokenService.getAuthName();
 
-  if(window.localStorage.getItem('cryptopal-client-auth-token')){
+  if(window.sessionStorage.getItem('cryptopal-client-auth-token')){
     navbarItems = 
       <ul>
-        <li><Link to="/dashboard">Dashboard</Link></li>
-        <li><Link to="/account">Account</Link></li>
+        <li><a href="#" aria-haspopup="true"><img src={ gravatarURL } alt={ userName } /> <span className="name">{ userName } <FontAwesomeIcon icon={faCaretDown} /></span></a>
+          <ul className="dropdown" aria-label="submenu">
+            <li><Link to="/dashboard">Dashboard</Link></li>
+            <li><Link to="/account">Edit Account</Link></li>
+            <li><Link to="/logout" onClick={logout}>Logout</Link></li>
+          </ul>
+        </li>
       </ul>
   }else{
     navbarItems = 
@@ -20,16 +30,24 @@ function Navbar() {
       </ul>
   }
 
+  function logout(event){
+    event.preventDefault();
+    TokenService.clearAuthData();
+    props.history.push('/')
+  }
+
   return(
-    <nav>
-      <div className="top-bar-left">
-        <Link to="/"><img src="" alt="Logo"/></Link>
-      </div>
-      <div className="top-bar-right">
-        {navbarItems}
+    <nav role="navigation">
+      <div className="container">
+        <div className="top-bar-left">
+          <Link to="/"><img src="" alt="Logo"/></Link>
+        </div>
+        <div className="top-bar-right">
+          {navbarItems}
+        </div>
       </div>
     </nav>
   )
 }
 
-export default Navbar;
+export default withRouter(Navbar);
