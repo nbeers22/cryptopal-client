b2bc4ad04a1e320ee238cb00f819c6bd0572a96d
nbeers22 <nbeers22@gmail.com>
2019-07-31 13:37:39 -0400

    Protected route from users that are not logged in

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,7 +1,24 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+import TokenService from '../../services/token-service';
 
 export default class Dashboard extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      loggedIn: false
+    }
+    const token = TokenService.getAuthToken();
+    if(token){
+      this.setState({ login: true })
+    }
+  }
+
   render() {
+    if(!this.state.loggedIn){
+      return <Redirect to="/login" />
+    }
     return (
       <div>
         Welcome, this is your dashboard
