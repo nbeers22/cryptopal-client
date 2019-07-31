f6976b6d7eabbbe795fa8c60d063b9fe03b9d967
nbeers22 <nbeers22@gmail.com>
2019-07-31 13:37:14 -0400

    Added new functions to get all different sessionStorage variables

--- src/services/token-service.js
+++ src/services/token-service.js
@@ -2,15 +2,28 @@ import config from '../config'
 
 const TokenService = {
   saveAuthData(data) {
-    window.localStorage.setItem(config.TOKEN_KEY, data.authToken)
-    window.localStorage.setItem("user_id", data.user_id)
-    window.localStorage.setItem("name", data.name)
+    window.sessionStorage.setItem(config.TOKEN_KEY, data.authToken)
+    window.sessionStorage.setItem("user_id", data.user_id)
+    window.sessionStorage.setItem("name", data.name)
+    window.sessionStorage.setItem("gravatar", data.gravatar)
   },
   getAuthToken() {
-    return window.localStorage.getItem(config.TOKEN_KEY)
+    return window.sessionStorage.getItem(config.TOKEN_KEY)
   },
-  clearAuthToken() {
-    window.localStorage.removeItem(config.TOKEN_KEY)
+  getAuthName() {
+    return window.sessionStorage.getItem("name")
+  },
+  getAuthUserID() {
+    return window.sessionStorage.getItem("user_id")
+  },
+  getAuthGravatar() {
+    return window.sessionStorage.getItem("gravatar")
+  },
+  clearAuthData() {
+    window.sessionStorage.removeItem(config.TOKEN_KEY)
+    window.sessionStorage.removeItem("user_id")
+    window.sessionStorage.removeItem("gravatar")
+    window.sessionStorage.removeItem("name")
   },
   hasAuthToken() {
     return !!TokenService.getAuthToken()
