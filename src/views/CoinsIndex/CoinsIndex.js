6f4c76134334d780106884e8b8a6967aeeae47d4
nbeers22 <nbeers22@gmail.com>
2019-08-26 11:56:42 -0400

    Removed unnecessary container wrapping component

--- src/views/CoinsIndex/CoinsIndex.js
+++ src/views/CoinsIndex/CoinsIndex.js
@@ -4,9 +4,6 @@ import CoinList from "../../components/CoinList/CoinList.js"
 export default function CoinsIndex() {
   return (
     <div>
-      <div className="container">
-        <h1>Coin List</h1>
-      </div>
       <CoinList />
     </div>
   )
