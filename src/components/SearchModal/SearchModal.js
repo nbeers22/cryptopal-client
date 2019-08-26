e7a2d693c98ccbdfced287d26ea92101583f6a26
nbeers22 <nbeers22@gmail.com>
2019-08-26 11:55:23 -0400

    Inserted accessibility features for modal

--- src/components/SearchModal/SearchModal.js
+++ src/components/SearchModal/SearchModal.js
@@ -3,14 +3,16 @@ import SearchCoins from '../SearchCoins/SearchCoins.js';
 import './SearchModal.css';
 
 export default class SearchModal extends Component {
+
   render() {
     return (
-      <div className={"SearchModal" + this.props.class}>
+      <div className={"SearchModal" + this.props.class} aria-modal="true" aria-labelledby="Search Modal" tabIndex="-1">
         <div className="md-content">
           <SearchCoins addFavorite={this.props.addFavorite} />
           <button
             className="close-button"
-            onClick={this.props.closeModal}>X
+            onClick={this.props.closeModal}
+            aria-label="Close Modal">X
           </button>
         </div>
       </div>
